f89efcf47d303b7c2082e6be7afd1d3dcdab9ccb
PJalv <134026493+PJalv@users.noreply.github.com>
2026-08-03 10:54:34 -0700

    t3code: run desktop without FHS sandbox

--- package.nix
+++ package.nix
@@ -1,12 +1,14 @@
-{ lib
-, appimageTools
-, asar
-, fetchurl
-, makeWrapper
-, runCommand
-, codex
-, sourceAssets
-, disableCheckpoints ? true
+{
+  lib,
+  appimageTools,
+  asar,
+  fetchurl,
+  makeWrapper,
+  pkgs,
+  runCommand,
+  codex,
+  sourceAssets,
+  disableCheckpoints ? true,
 }:
 
 let
@@ -21,40 +23,67 @@ let
   appimageContents = appimageTools.extractType2 {
     inherit pname version src;
   };
-  sourceAppimageContents = runCommand "${pname}-${version}-source-patched" {
-    nativeBuildInputs = [ asar ];
-  } ''
-    mkdir -p "$out"
-    cp -a ${appimageContents}/. "$out/"
-    chmod -R u+w "$out/resources"
+  sourceAppimageContents =
+    runCommand "${pname}-${version}-source-patched"
+      {
+        nativeBuildInputs = [ asar ];
+      }
+      ''
+        mkdir -p "$out"
+        cp -a ${appimageContents}/. "$out/"
+        chmod -R u+w "$out/resources"
 
-    mkdir app
-    asar extract "$out/resources/app.asar" app
+        mkdir app
+        asar extract "$out/resources/app.asar" app
 
-    rm -rf app/apps/server/dist app/apps/desktop/dist-electron
-    cp -r ${sourceAssets}/apps/server/dist app/apps/server/
-    cp -r ${sourceAssets}/apps/desktop/dist-electron app/apps/desktop/
+        rm -rf app/apps/server/dist app/apps/desktop/dist-electron
+        cp -r ${sourceAssets}/apps/server/dist app/apps/server/
+        cp -r ${sourceAssets}/apps/desktop/dist-electron app/apps/desktop/
 
-    rm -f "$out/resources/app.asar"
-    rm -rf "$out/resources/app.asar.unpacked"
-    asar pack \
-      --unpack-dir 'node_modules/{@ff-labs,@msgpackr-extract,@yuuang,node-pty}' \
-      app \
-      "$out/resources/app.asar"
+        rm -f "$out/resources/app.asar"
+        rm -rf "$out/resources/app.asar.unpacked"
+        asar pack \
+          --unpack-dir 'node_modules/{@ff-labs,@msgpackr-extract,@yuuang,node-pty}' \
+          app \
+          "$out/resources/app.asar"
 
-    test -f "$out/resources/app.asar"
-    test -d "$out/resources/app.asar.unpacked/node_modules/node-pty"
-  '';
-  checkpointWrapperArgs = lib.optionalString disableCheckpoints
-    "--set T3_DISABLE_CHECKPOINTS 1";
+        test -f "$out/resources/app.asar"
+        test -d "$out/resources/app.asar.unpacked/node_modules/node-pty"
+      '';
+  checkpointWrapperArgs = lib.optionalString disableCheckpoints "--set T3_DISABLE_CHECKPOINTS 1";
+  runtimeLibraryPath = lib.makeLibraryPath (
+    appimageTools.defaultFhsEnvArgs.targetPkgs pkgs ++ appimageTools.defaultFhsEnvArgs.multiPkgs pkgs
+  );
 in
-appimageTools.wrapAppImage {
-  inherit pname version;
-  contents = sourceAppimageContents;
-  nativeBuildInputs = [ makeWrapper ];
+runCommand "${pname}-${version}"
+  {
+    nativeBuildInputs = [ makeWrapper ];
 
-  extraInstallCommands = ''
-    mkdir -p "$out/share"
+    passthru = {
+      inherit
+        codex
+        disableCheckpoints
+        sourceAppimageContents
+        sourceAssets
+        ;
+      release = source;
+    };
+
+    meta = {
+      description = "Latest T3 Code nightly with a bundled Codex CLI";
+      homepage = "https://github.com/pingdotgg/t3code";
+      changelog = "https://github.com/pingdotgg/t3code/releases/tag/v${version}";
+      license = lib.licenses.mit;
+      mainProgram = pname;
+      platforms = [ "x86_64-linux" ];
+      sourceProvenance = with lib.sourceTypes; [
+        fromSource
+        binaryNativeCode
+      ];
+    };
+  }
+  ''
+    mkdir -p "$out/bin" "$out/share"
 
     if [ -d ${sourceAppimageContents}/usr/share ]; then
       cp -r ${sourceAppimageContents}/usr/share/* "$out/share/"
@@ -69,38 +98,21 @@ appimageTools.wrapAppImage {
         -e 's|TryExec=AppRun|TryExec=${pname}|g' \
         -e 's|^StartupWMClass=.*$|StartupWMClass=t3-code-desktop|g' \
         "$desktop_file"
+    fi
 
+    makeWrapper ${sourceAppimageContents}/AppRun "$out/bin/${pname}" \
+      --add-flags "--ignore-certificate-errors" \
+      ${checkpointWrapperArgs} \
+      --prefix LD_LIBRARY_PATH : "${runtimeLibraryPath}" \
+      --prefix XDG_DATA_DIRS : "$out/share" \
+      --prefix PATH : "${lib.makeBinPath [ codex ]}"
+
+    if [ -n "$desktop_file" ]; then
       wrapProgram "$out/bin/${pname}" \
-        --add-flags "--ignore-certificate-errors" \
-        --set CHROME_DESKTOP "$desktop_basename" \
-        ${checkpointWrapperArgs} \
-        --prefix XDG_DATA_DIRS : "$out/share" \
-        --prefix PATH : "${lib.makeBinPath [ codex ]}"
-    else
-      wrapProgram "$out/bin/${pname}" \
-        --add-flags "--ignore-certificate-errors" \
-        ${checkpointWrapperArgs} \
-        --prefix XDG_DATA_DIRS : "$out/share" \
-        --prefix PATH : "${lib.makeBinPath [ codex ]}"
+        --set CHROME_DESKTOP "$desktop_basename"
     fi
 
     if [ -f ${sourceAppimageContents}/.DirIcon ]; then
       install -Dm444 ${sourceAppimageContents}/.DirIcon "$out/share/pixmaps/${pname}.png"
     fi
-  '';
-
-  passthru = {
-    inherit codex disableCheckpoints sourceAppimageContents sourceAssets;
-    release = source;
-  };
-
-  meta = {
-    description = "Latest T3 Code nightly with a bundled Codex CLI";
-    homepage = "https://github.com/pingdotgg/t3code";
-    changelog = "https://github.com/pingdotgg/t3code/releases/tag/v${version}";
-    license = lib.licenses.mit;
-    mainProgram = pname;
-    platforms = [ "x86_64-linux" ];
-    sourceProvenance = with lib.sourceTypes; [ fromSource binaryNativeCode ];
-  };
-}
+  ''
